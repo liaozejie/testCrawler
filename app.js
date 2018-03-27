5c7797b92b27e2032aefd4f995a15cdbb93e4543
liaozejie <1035123023@qq.com>
2018-03-27 14:51:23 +0800

    testCrawler commit

--- app.js
+++ app.js
@@ -9,6 +9,22 @@ superagent.get(targetUrl)
             return console.error(err);
         }
         var array = [];
+        console.log(res.text);
         var $ = cheerio.load(res.text);
-        $()
+        var gsmc = $("td.gsmc a");
+        var zwyx = $("td.zwyx");
+        var gzdd = $("td.gzdd");
+        var gxsj = $("td.gxsj span");
+        var len = gsmc.length;
+        var objArray = [];
+        for(var i = 0;i<len;i++){
+            var obj = {
+                "公司":gsmc.eq(i).text(),
+                "薪资范围":zwyx.eq(i).text(),
+                "工作地点":gzdd.eq(i).text(),
+                "发布状态":gxsj.eq(i).text()
+            }
+            objArray.push(obj);
+        }
+        console.log(objArray);
     });
