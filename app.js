ea166728f8ce79bac63bfe89c30437eb6f7385d4
liaozejie <1035123023@qq.com>
2018-03-28 17:38:12 +0800

    commit

--- app.js
+++ app.js
@@ -13,12 +13,14 @@ superagent.get(targetUrl)
             return console.error(err);
         }
         var array = [];
+        console.log(res.text);
         var $ = cheerio.load(res.text);
         var zwmc = $("td.zwmc div a");
         var gsmc = $("td.gsmc a");
         var zwyx = $("td.zwyx");
         var gzdd = $("td.gzdd");
         var gxsj = $("td.gxsj span");
+        var newlist_detail = $(".newlist_detail .newlist_deatil_two");
         var len = gsmc.length;
         for(var i = 0;i<len;i++){
             if(gsmc.eq(i).text()&&zwmc.eq(i).text()&&zwyx.eq(i).text()&&gzdd.eq(i).text()&&gxsj.eq(i).text()){
@@ -27,10 +29,19 @@ superagent.get(targetUrl)
                     "职位":zwmc.eq(i).text(),
                     "薪资范围":zwyx.eq(i).text(),
                     "工作地点":gzdd.eq(i).text(),
-                    "发布时间":new Date()
+                    "发布时间":new Date(),
+                    "详情":{
+                        "地点":newlist_detail.eq(i).find("span").eq(0).text(),
+                        "公司性质":newlist_detail.eq(i).find("span").eq(1).text(),
+                        "公司规模":newlist_detail.eq(i).find("span").eq(2).text(),
+                        "学历要求":newlist_detail.eq(i).find("span").eq(3).text(),
+                        "职位薪资":newlist_detail.eq(i).find("span").eq(4).text(),
+                        "岗位职责":newlist_detail.eq(i).find("li").eq(0).text()
+                    }
                 }
+                console.log(obj);
                 db.insertOne(dburl,dbName,collection,obj,function(err,result){
-                    console.log(result);
+
                 })
             }
         }
